64b5efbd9fadd6f932a2ea2a94fb9fc60cdd7e44
Nozo <nozomi.pine@gmail.com>
2021-08-05 12:24:05 +0000

    Move functions to main scope.

--- src/main.ts
+++ src/main.ts
@@ -132,149 +132,149 @@ window.onload = () => {
   };
 
   drawScene(gl, uniformLocations, cube_vao);
-};
 
-function initShaderProgram(
-  gl: WebGL2RenderingContext,
-  vsSource: string,
-  fsSource: string
-): WebGLProgram | null {
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+  function initShaderProgram(
+    gl: WebGL2RenderingContext,
+    vsSource: string,
+    fsSource: string
+  ): WebGLProgram | null {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
-  if (!vertexShader || !fragmentShader) {
-    return null;
+    if (!vertexShader || !fragmentShader) {
+      return null;
+    }
+
+    const shaderProgram = gl.createProgram();
+
+    if (!shaderProgram) {
+      return null;
+    }
+
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+      console.log(
+        "Unable to initialize the shader program: ",
+        gl.getProgramInfoLog(shaderProgram)
+      );
+      return null;
+    }
+
+    gl.useProgram(shaderProgram);
+
+    return shaderProgram;
   }
 
-  const shaderProgram = gl.createProgram();
+  function loadShader(
+    gl: WebGL2RenderingContext,
+    type: number,
+    source: string
+  ): WebGLShader | null {
+    const shader = gl.createShader(type);
 
-  if (!shaderProgram) {
-    return null;
+    if (!shader) {
+      console.log("An error occurred creating the shaders: ", type);
+      return null;
+    }
+
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      console.log(
+        "An error occurred compiling the shaders: ",
+        gl.getShaderInfoLog(shader)
+      );
+      gl.deleteShader(shader);
+      return null;
+    }
+
+    return shader;
   }
 
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
+  function create_vao(
+    gl: WebGL2RenderingContext,
+    attrs: {
+      buffer: number[];
+      index: number;
+      size: number;
+    }[],
+    index_buffer: number[]
+  ): WebGLVertexArrayObject | null {
+    const vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
 
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    console.log(
-      "Unable to initialize the shader program: ",
-      gl.getProgramInfoLog(shaderProgram)
-    );
-    return null;
+    attrs.map((attr) => {
+      const vbo = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(attr.buffer),
+        gl.STATIC_DRAW
+      );
+      gl.enableVertexAttribArray(attr.index);
+      gl.vertexAttribPointer(attr.index, attr.size, gl.FLOAT, false, 0, 0);
+    });
+
+    if (index_buffer) {
+      const ibo = gl.createBuffer();
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+      gl.bufferData(
+        gl.ELEMENT_ARRAY_BUFFER,
+        new Uint16Array(index_buffer),
+        gl.STATIC_DRAW
+      );
+    }
+
+    gl.bindVertexArray(null);
+    return vao;
   }
 
-  gl.useProgram(shaderProgram);
+  function drawScene(
+    gl: WebGL2RenderingContext,
+    uniformLocations: {
+      mvpMatrix: WebGLUniformLocation | null;
+    },
+    target: {
+      vao: WebGLVertexArrayObject | null;
+      idx_len: number;
+    }
+  ) {
+    const mMatrix = Mat4.identity(Mat4.create());
+    const vMatrix = Mat4.identity(Mat4.create());
+    const pMatrix = Mat4.identity(Mat4.create());
+    const vpMatrix = Mat4.identity(Mat4.create());
+    const mvpMatrix = Mat4.identity(Mat4.create());
 
-  return shaderProgram;
-}
+    Mat4.lookAt([0.0, 1.0, 5.0], [0, 0, 0], [0, 1, 0], vMatrix);
+    Mat4.perspective(45, gl.canvas.width / gl.canvas.height, 0.1, 100, pMatrix);
+    Mat4.multiply(pMatrix, vMatrix, vpMatrix);
 
-function loadShader(
-  gl: WebGL2RenderingContext,
-  type: number,
-  source: string
-): WebGLShader | null {
-  const shader = gl.createShader(type);
+    gl.enable(gl.DEPTH_TEST);
 
-  if (!shader) {
-    console.log("An error occurred creating the shaders: ", type);
-    return null;
-  }
+    function render(timestamp: number) {
+      const t = timestamp / 1000.0;
 
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
+      gl.clearColor(0.0, 0.0, 0.0, 1.0);
+      gl.clearDepth(1.0);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.log(
-      "An error occurred compiling the shaders: ",
-      gl.getShaderInfoLog(shader)
-    );
-    gl.deleteShader(shader);
-    return null;
-  }
+      Mat4.identity(mMatrix);
+      Mat4.rotate(mMatrix, t, [0.0, 1.0, 0.0], mMatrix);
+      Mat4.rotate(mMatrix, t, [1.0, 1.0, 0.0], mMatrix);
+      Mat4.multiply(vpMatrix, mMatrix, mvpMatrix);
+      gl.uniformMatrix4fv(uniformLocations.mvpMatrix, false, mvpMatrix);
 
-  return shader;
-}
+      gl.bindVertexArray(target.vao);
+      gl.drawElements(gl.TRIANGLES, target.idx_len, gl.UNSIGNED_SHORT, 0);
 
-function create_vao(
-  gl: WebGL2RenderingContext,
-  attrs: {
-    buffer: number[];
-    index: number;
-    size: number;
-  }[],
-  index_buffer: number[]
-): WebGLVertexArrayObject | null {
-  const vao = gl.createVertexArray();
-  gl.bindVertexArray(vao);
+      gl.flush();
 
-  attrs.map((attr) => {
-    const vbo = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-    gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array(attr.buffer),
-      gl.STATIC_DRAW
-    );
-    gl.enableVertexAttribArray(attr.index);
-    gl.vertexAttribPointer(attr.index, attr.size, gl.FLOAT, false, 0, 0);
-  });
-
-  if (index_buffer) {
-    const ibo = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-    gl.bufferData(
-      gl.ELEMENT_ARRAY_BUFFER,
-      new Uint16Array(index_buffer),
-      gl.STATIC_DRAW
-    );
-  }
-
-  gl.bindVertexArray(null);
-  return vao;
-}
-
-function drawScene(
-  gl: WebGL2RenderingContext,
-  uniformLocations: {
-    mvpMatrix: WebGLUniformLocation | null;
-  },
-  target: {
-    vao: WebGLVertexArrayObject | null;
-    idx_len: number;
-  }
-) {
-  const mMatrix = Mat4.identity(Mat4.create());
-  const vMatrix = Mat4.identity(Mat4.create());
-  const pMatrix = Mat4.identity(Mat4.create());
-  const vpMatrix = Mat4.identity(Mat4.create());
-  const mvpMatrix = Mat4.identity(Mat4.create());
-
-  Mat4.lookAt([0.0, 1.0, 5.0], [0, 0, 0], [0, 1, 0], vMatrix);
-  Mat4.perspective(45, gl.canvas.width / gl.canvas.height, 0.1, 100, pMatrix);
-  Mat4.multiply(pMatrix, vMatrix, vpMatrix);
-
-  gl.enable(gl.DEPTH_TEST);
-
-  function render(timestamp: number) {
-    const t = timestamp / 1000.0;
-
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clearDepth(1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    Mat4.identity(mMatrix);
-    Mat4.rotate(mMatrix, t, [0.0, 1.0, 0.0], mMatrix);
-    Mat4.rotate(mMatrix, t, [1.0, 1.0, 0.0], mMatrix);
-    Mat4.multiply(vpMatrix, mMatrix, mvpMatrix);
-    gl.uniformMatrix4fv(uniformLocations.mvpMatrix, false, mvpMatrix);
-
-    gl.bindVertexArray(target.vao);
-    gl.drawElements(gl.TRIANGLES, target.idx_len, gl.UNSIGNED_SHORT, 0);
-
-    gl.flush();
-
+      requestAnimationFrame(render);
+    }
     requestAnimationFrame(render);
   }
-  requestAnimationFrame(render);
-}
+};
